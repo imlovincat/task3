3312106fa9fa38f05353a7c95f9074514aabb14f
Chi Ieong Ng <imlovincat@gmail.com>
2022-03-29 11:15:49 +0100

    TDD RED: write a test named "specificationUpdateStaff" and test failed

--- tests/cm/NgChiIeongTestTask3.java
+++ tests/cm/NgChiIeongTestTask3.java
@@ -151,5 +151,21 @@ public class NgChiIeongTestTask3 {
         assertEquals(new BigDecimal(14.88,new MathContext(4)),cost);
     }
 
+    @Test
+    public void specificationUpdateStaff() throws IllegalArgumentException {
+
+        CarParkKind kind = CarParkKind.STAFF;
+        BigDecimal hourlyNormalRate = new BigDecimal(5);
+        BigDecimal hourlyReducedRate = new BigDecimal(3);
+        ArrayList<Period> normalPeriods = new ArrayList<Period>();
+        ArrayList<Period> reducedPeriods = new ArrayList<Period>();
+        Collections.addAll(normalPeriods,new Period(9,13), new Period(14,18));
+        Collections.addAll(reducedPeriods,new Period(7,9),new Period(13,14),new Period(18,22));
+        Rate rate = new Rate(kind,hourlyNormalRate,hourlyReducedRate,reducedPeriods,normalPeriods);
+        Period periodStay = new Period(0,24);
+        BigDecimal cost = rate.calculate(periodStay);
+        assertEquals(new BigDecimal(16),cost);
+    }
+
 
 }
